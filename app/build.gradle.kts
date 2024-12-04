8ada27b3b788f31fc9621c7a54e3789c7f545079
ri0103 <ryu2006ishizaki@gmail.com>
2024-12-04 18:09:13 +0900

    Firebase, Open AI Apiの読み込み

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-analytics")
-//    implementation("com.google.firebase:firebase-database-ktx")
     implementation("com.google.firebase:firebase-firestore")
     implementation ("com.google.firebase:firebase-auth")
     implementation ("com.google.android.gms:play-services-auth:20.6.0")
